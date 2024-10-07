7c1b63c165006996017cfc23ec287773332bdc36
Roskoshenko Maksym <maks.roskoshenko@gmail.com>
2024-10-07 23:15:17 +0300

    created functions to split words and to split sentences

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,25 @@ public class Main {
         String[][] words;
         int maxFreq = 0;
         String mostFreqWord = "";
+
+        try {
+            String inputText = text.toString();
+
+            sentences = splitSentences(inputText);
+            words = new String[sentences.length][];
+
+            for (int i = 0; i < sentences.length; i++) {
+                words[i] = splitWords(sentences[i].toLowerCase());
+            }
+        }
+    }
+
+    private String[] splitSentences(String text) {
+        return text.split("[.!?]");
+    }
+
+    private String[] splitWords(String sentence) {
+        return sentence.split("[ ,;]");
     }
 
     public static void main(String[] args) {
