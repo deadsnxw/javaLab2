61a783ad35cb0a98c47f0f18fbe59e0442aa0da1
Roskoshenko Maksym <maks.roskoshenko@gmail.com>
2024-10-07 23:06:40 +0300

    started to creating function by lab task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+    public void maxSentence(StringBuffer text) {
+        String[] sentences;
+        String[][] words;
+        int maxFreq = 0;
+        String mostFreqWord = "";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
